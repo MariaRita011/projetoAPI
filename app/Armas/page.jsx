9a15de9ece75c2bb4152df39ce1a890715e561f0
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2023-10-16 16:33:19 -0300

    feat: inicio css

--- app/Armas/page.jsx
+++ app/Armas/page.jsx
@@ -2,15 +2,24 @@
 
 import React, { useEffect, useState } from 'react'
 import Cards from '@/data/armas';
+import styles from './armas.module.css'
 
+import Image from 'next/image';
 function page() {
 
     const [apiData, setApiData] = useState('');
+    const [language, setLanguage] = useState("portugues");
+
+    function lingua() {
+        language == 'portugues' ? setLanguage('ingles') : setLanguage('portugues')
+        console.log(language);
+    }
+
 
     useEffect(() => {
         const CardsFecth = async () => {
             try {
-                const dados = await Cards();
+                const dados = await Cards(language);
                 setApiData(dados)
                 console.log(dados);
             } catch (error) {
@@ -18,19 +27,23 @@ function page() {
             }
         }
         CardsFecth();
-    }, [])
+    }, [language])
 
     return (
-        <div>
+        <div className={styles.main}>
+            <button onClick={lingua}>{language}</button>
+
+            <Image  className={styles.image} src={'/image (2).png'} width={1495} height={600}/>
             <h2>Teste</h2>
-            <div>
+            <div className={styles.container}>
                 {
                     apiData ? (
                         apiData.map((card) => (
-                            <div key={card.id}>
+                            <div className={styles.card} key={card.id}>
                                 <h1>{card.name}</h1>
                                 <p>{card.description}</p>
-                              <img src={card.image} alt={card.name} width={264} height={264} /> 
+                      
+                                <img src={card.image} alt={card.name} width={264} height={264} />
                             </div>
 
                         ))
--- data/armas.js
+++ data/armas.js
@@ -2,11 +2,14 @@
 
 import axios from "axios";
 
-const URl = 'https://bymykel.github.io/CSGO-API/api/pt-BR/agents.json';
 
-const Card = async () => {
+
+const Card = async (language) => {
+    const teste = language == 'portugues' ? 'pt-BR' : 'en'
+    console.log(teste);
+    const URL = `https://bymykel.github.io/CSGO-API/api/${teste}/agents.json`
     try {
-        const resposta = await axios.get(URl);
+        const resposta = await axios.get(URL);
         return resposta.data;
     } catch (error) {
         throw error;
