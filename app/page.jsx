df0431de6bf69b664eab98fee4f0006b74a7f1ad
Liviadbelao <125027552+Liviadbelao@users.noreply.github.com>
2023-11-05 00:44:56 -0300

    feat: css login

--- app/page.jsx
+++ app/page.jsx
@@ -16,68 +16,84 @@ const Home = () => {
   const [erro, setErro] = useState(false)
   const [msg, setMsg] = useState(false)
   const [sucesso, setSucesso] = useState(false)
-const [link, setLink] = useState('')
+  const [link, setLink] = useState('')
   const lista = new List()
   function adicionar() {
-    if (nome.trim() == '' || data.trim() == '' || tell.trim() == '' ) {
+    if (nome.trim() == '' || data.trim() == '' || tell.trim() == '') {
       setErro(true)
       setTimeout(() => {
         setErro(false)
       }, 3000)
       console.log('n passou pelos inputs vazios')
- 
+
     } else {
       console.log('adicionou');
       setSucesso(true)
       setTimeout(() => {
         setSucesso(false)
-    }, 3000)
+      }, 3000)
       const usuario = new Login(nome, data, tell)
       lista.add(usuario)
       console.log(lista)
       setLink('/paginahome')
     }
   }
- 
+
 
   return (
     <main className={styles.main}>
 
-      <h1  className={styles.title}>Faça seu login antes de entrar no site!</h1>
-      <div className={styles.containerLogin}>
+      <div className={styles.sec}> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span> <span></span>
+        <div className={styles.signin}>
+          <div className={styles.container}>
+            
+              <h1 className={styles.title}>Login</h1>
+              <div className={styles.form}>
+              <div className={styles.inputBox}>
+                <Inputs type={'text'} valor={nome} name={'nome'} ph={"Digite seu nome"} on={(e) => setNome(e.target.value)} />
+              </div>
+              <div className={styles.inputBox}>
+                <Inputs type={'date'} valor={data} name={'data'} ph={"Digite sua data de aniverário"} on={(e) => setData(e.target.value)} />
+              </div>
+              <div className={styles.inputBox}>
+                <Inputs type={'number'} valor={tell} name={'tell'} ph={"Digite seu telefone"} on={(e) => setTell(e.target.value)} />
+              </div>
+              <div className={styles.inputBox}>
+                <button className={styles.button} onClick={adicionar}>
+                  <Link className={styles.link} href={link}>Cadastre-se</Link>
 
-      <Inputs type={'text'} valor={nome} name={'nome'} ph={"Digite seu nome"} on={(e) => setNome(e.target.value)}/>
-      <Inputs type={'date'} valor={data} name={'data'} ph={"Digite sua data de aniverário"} on={(e) => setData(e.target.value)}/>
-      <Inputs type={'number'} valor={tell} name={'tell'} ph={"Digite seu telefone"} on={(e) => setTell(e.target.value)}/>
-
-     <div className={styles.buttonContainer}>
-      <button className={styles.button} onClick={adicionar}>
-      Cadastre-se
-      </button>
+                </button>
 
 
-      <button className={styles.button} >
-        <Link  className={styles.link} href={link}>Visite nosso site</Link>
-      </button>
+
+              </div>
+              <div className={styles.msg}>
+              {//mensagem de erro
+
+                erro ? <NavMsg tipo={"erro"} msg={'Preecha todos os campos'} /> : null
+
+              }
+              {//mensagem de erro
+
+                msg ? <NavMsg tipo={"erro"} msg={'Você não pode visitar o site sem ter feito o cadastro antes'} /> : null
+
+              }
+              {//mensagem de erro
+
+                sucesso ? <NavMsg tipo={"sucesso"} msg={'Parabéns, login feito com sucesso!'} /> : null
+
+              }
+              </div>
+            </div>
+
+
+          </div>
+
+        </div>
+
+
       </div>
 
-      </div>
-
-      {//mensagem de erro
-
-        erro ? <NavMsg tipo={"erro"} msg={'Preecha todos os campos'} /> : null
-
-      }
-      {//mensagem de erro
-
-        msg ? <NavMsg tipo={"erro"} msg={'Você não pode visitar o site sem ter feito o cadastro antes'} /> : null
-
-      }
-      {//mensagem de erro
-
-        sucesso ? <NavMsg tipo={"sucesso"} msg={'Parabéns, login feito com sucesso!'} /> : null
-
-      }
     </main>
   )
 }
